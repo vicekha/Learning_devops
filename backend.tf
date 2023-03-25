b64b22fd15c9ff308a162a2a3c5c612d9a960575
vicekha <youville@yandex.com>
2023-03-24 20:48:14 -0400

    moduled fixed

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
- backend "gcs" {
-   bucket  = "7dddb4201524b3f9-bucket-tfstate"
-   prefix  = "terraform/state"
- }
+  backend "gcs" {
+    bucket = "7dddb4201524b3f9-bucket-tfstate"
+    prefix = "terraform/state"
+  }
 }
--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
 module "website_s3_bucket" {
-  source = "Learning_devops/modules/google_instance"
+  source       = "./modules/google_instance"
+  name         = "testing_jenkins!"
+  project      = "logical-vim-376815"
+  region       = "us-central1"
+  account_id   = "c-9394"
+  display_name = "display_name"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+  image        = "ubuntu-os-cloud/ubuntu-minimal-1804-lts"
+  NAME         = "youvilles-ip"
+  ADDRESS_TYPE = "EXTERNAL"
+  DNS-NAME     = "youville.com."
+  MANAGED_ZONE = "devops-villa.com"
+
+
 }
